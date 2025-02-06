d757d3cd3a49fc6ff83f9f62fba29992282cb641
frazrepo <frazvisuals@gmail.com>
2025-02-06 19:27:26 +0100

    Add mini align and mini operators

---
+++ lua/plugins/mini-align.lua
@@ -0,0 +1,11 @@
+    -- mini.Align - ga simple and gA interactive
+    {
+        'echasnovski/mini.align',
+        vscode = true,
+        opts = {},
+        keys = {
+            { "ga", desc = "Align" },
+            { "gA", desc = "Align interactive" },
+            { "ga", mode = "x" },
+        },
+    },
---
+++ lua/plugins/mini-operators.lua
@@ -0,0 +1,14 @@
+    -- minioperators  : Sort (gs), Replace with register (gr), Exchange text regions (gx/gxx), Multiply text (gm/gmm),
+    {
+        'echasnovski/mini.operators',
+         version = '*' ,
+         vscode = true,
+         opts = {},
+         keys = {
+            { "gs" , desc= "Sort text region" },
+            { "gx" , desc= "Exchange text region" },
+            { "gm" , desc= "Multiply text region" },
+            { "gr" , desc= "Replace with register" },
+            -- { "g=" , "Evaluate text and replace" }, -- Already used to indent buffer
+         }
+    },
